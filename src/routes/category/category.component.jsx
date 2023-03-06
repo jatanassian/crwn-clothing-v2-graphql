4758a11d678cf945729386c09dbabf1fb3185496
Julien Atanassian <julien.atanassian@gmail.com>
2023-03-05 22:36:23 -0500

    add gql query category by title

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,21 +1,43 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 
 import ProductCard from '../../components/product-card/product-card.component';
 import Spinner from '../../components/spinner/spinner.component';
 
-import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
 
+const GET_CATEGORY = gql`
+query($title: String!) {
+	getCollectionsByTitle(title: $title) {
+	  id
+	  title
+	  items {
+		id
+		name
+		price
+		imageUrl
+	  }
+	}
+  }
+`;
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+	const { loading, error, data } = useQuery(GET_CATEGORY, { variables: { title: category }});
+
+  const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		if (data) {
+			const {
+				getCollectionsByTitle: { items }
+			} = data;
+			setProducts(items)
+		}
+	})
 
   return (
     <Fragment>
